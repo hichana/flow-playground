9cc29c52891eaf85acde075c9249abf454c23d5b
Matt Chana <8943047+hichana@users.noreply.github.com>
2021-10-09 11:49:00 +0900

    mock render first user's icon for milestone 2

--- src/components/ResourcesExplorerButton.tsx
+++ src/components/ResourcesExplorerButton.tsx
@@ -17,16 +17,19 @@ export const ResourcesExplorerButton = (props: ResourcesExplorerButtonProps) => 
 
   const location = useLocation();
   const params = getParams(location.search);
-  const { type, id } = params;  
+  // const { type, id } = params;  
+  const { type, id, storage } = params;  // Milestone 2: pull 'storage' param for rendering only first user icon
 
-  return project.id === 'LOCAL-project' ? null : (
+  // return project.id === 'LOCAL-project' ? null : (
+  return project.id !== 'LOCAL-project' ? null : ( // Milestone 2: set all to render to demonstrate UI
     <SidebarItemToggleResources
       onClick={() => {
         setSelectedResourceAccount(addr)
         navigate(`/${projectPath}?type=${type}&id=${id}&storage=${addr || 'none'}`)
       }}
       title={'Open the resources explorer'}
-      active={true}
+      // active={false}
+      active={storage === addr ? true : false} // Milestone 2: make only the first icon be in active state
     >
       <FaDatabase />
     </SidebarItemToggleResources>
